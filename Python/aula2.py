c3da4ab1f9f407d22db19437ee255f36e3435d28
Matheus <matheuswschneider@gmail.com>
2026-07-16 18:56:46 -0300

    Operações com Strings

---
+++ Python/aula2.py
@@ -0,0 +1,14 @@
+""" Operações com strings
+● str.capitalize() - Primeira letra caixa alta → str
+● str.upper() - Toda a string em caixa alta → str
+● str.lower() ou casefold() - Toda a string em caixa baixa → str
+● str.split(separator, maxsplit) - Separa de acordo com o separador
+definido → list
+● separator.str.join(iterable) - Une elementos de uma lista em uma string
+→ str
+● str.count(value, start, end) - Retorna quantidade de vezes que valor
+aparece em um intervalo numa string → int
+● str.replace(oldvalue, newvalue, count) - substitui um valor por outro na
+quantidade definida numa string → str """
+
+
