8940e103bbfc925e70cf6d2fc3d238986c1f87cd
Matheus <matheuswschneider@gmail.com>
2026-07-23 12:55:19 -0300

    Cloud

--- Mofulo7/aula7.py
+++ Mofulo7/aula7.py
@@ -9,6 +9,33 @@ interação do provedor de serviço. Esse modelo de nuvem é composto por
 cinco características essenciais, três modelos de serviço e quatro modelos
 de implantação.
 
+
+Cloud é computação como utilidade
+Você não compra a usina: você paga pela energia. Em cloud, você paga por
+compute/storage/rede conforme usa.
+O que NÃO é cloud
+“Tenho um servidor remotoˮ ≠ cloud necessariamente
+“Tenho um data center terceirizadoˮ ≠ cloud necessariamente
+ Cloud exige as características essenciais
+
+ CARACTERÍSTICAS ESSENCIAIS
+Auto serviço sob demanda
+Recursos são provisionados de acordo com a solicitação do cliente, sem intervenção do
+provedor
+
+Amplo acesso via rede
+Recursos são acessíveis via internet, à partir de diversas plataformas e dispositivos
+
+Agrupamento de recursos
+Capacidade de concentrar os recursos e ajustar o provisionamento de acordo com a demanda
+
+Consumo medido do serviço
+Recursos são cobrados de acordo com as métricas de utilização, apenas pelo que for usado
+
+Elasticidade rápida
+Mecanismos sistemicos de alta capacidade responsiva atrelado ao provisionamento de recursos
+
+
 DADOS DE BRASILEIROS NÃO PODEM SER ARMEZANADOS EM CLOUDS DE FORA (LEI BRASILEIRA)
 
 vantagens operacionais cloud:
@@ -18,5 +45,15 @@ CONTROLE DE ACESSO
 MONITORAMENTO 
 ORQUESTRAÇÃO
 
+PROVEDORES CLOUD
+Recursos e serviços prestados
+Temos acesso aos recursos e serviços da
+nuvem através dos provedores. Algumas
+das principais características inerentes
+aos serviços:
+● Conta
+● Regiões
+● Zonas
+● VPC*
 
 """
