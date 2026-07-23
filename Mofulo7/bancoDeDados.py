a400eaf2426935992691cca84e7d0dacc33ccb63
Matheus <matheuswschneider@gmail.com>
2026-07-23 19:13:14 -0300

    Banco de dados

---
+++ Mofulo7/bancoDeDados.py
@@ -0,0 +1,30 @@
+""" Conceito
+ORACLE
+“Um banco de dados é uma coleção organizada de informações - ou dados
+- estruturadas, normalmente armazenadas eletronicamente em um sistema de computador.ˮ
+
+Dados Estruturados: Dados organizados em um formato de
+banco de dados com linhas e colunas, com chaves relacionais.
+
+● Endereçáveis para análise eficaz
+● Facilmente mapeados em campos pré-designados
+
+Exemplo: Dados relacionais
+armazenados em um banco de
+dados SQL
+
+Dados Semi-Estruturados: Informações com propriedades
+organizacionais, porém não residem em um banco de dados relacional.
+
+● Facilitam a análise, embora possam ser desafiadores para
+armazenar em um banco de dados relacional
+Exemplo: Dados em formato JSON.
+
+Dados Não Estruturados: Dados sem organização pré-definida ou modelo de
+dados, não adequados para um banco dedados relacional.
+● Armazenados e gerenciados em plataformas alternativas
+● Cada vez mais utilizados em sistemas de TI
+
+Exemplo: Documentos em formato Word, PDF, texto, registros de mídia
+
+"""
