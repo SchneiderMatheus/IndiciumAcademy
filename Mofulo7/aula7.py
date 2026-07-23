b35037526c1d6b115d0e0b62cbe9465af76d2ef8
Matheus <matheuswschneider@gmail.com>
2026-07-23 12:50:23 -0300

    Moudulo 7

---
+++ Mofulo7/aula7.py
@@ -0,0 +1,22 @@
+""" Afinal, o que é?
+Cloud
+A computação em nuvem é um modelo que permite um acesso amplo,
+conveniente e sob demanda via rede, a um conjunto compartilhado de
+recursos de computação configuráveis (por exemplo, redes, servidores,
+armazenamento, aplicativos e serviços) que podem ser rapidamente
+provisionados e liberados com mínimo esforço de gerenciamento ou
+interação do provedor de serviço. Esse modelo de nuvem é composto por
+cinco características essenciais, três modelos de serviço e quatro modelos
+de implantação.
+
+DADOS DE BRASILEIROS NÃO PODEM SER ARMEZANADOS EM CLOUDS DE FORA (LEI BRASILEIRA)
+
+vantagens operacionais cloud:
+PROVISIONAMENTO
+CONECTIVIDADE
+CONTROLE DE ACESSO
+MONITORAMENTO 
+ORQUESTRAÇÃO
+
+
+"""
