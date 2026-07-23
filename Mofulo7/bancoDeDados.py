a7798b5b7ec83f64a40521a87fb1dfa5e5c83d21
Matheus <matheuswschneider@gmail.com>
2026-07-23 19:28:03 -0300

    SGBD

--- Mofulo7/bancoDeDados.py
+++ Mofulo7/bancoDeDados.py
@@ -27,4 +27,26 @@ dados, não adequados para um banco dedados relacional.
 
 Exemplo: Documentos em formato Word, PDF, texto, registros de mídia
 
+
+O que são SGBD?
+
+Um sistema de gerenciamento de
+banco de dados (SGBD) é um
+conjunto de programas de software
+que permite aos usuários criar,
+editar, atualizar, armazenar e
+recuperar dados em tabelas de
+banco de dados.
+
+ARQUITETURA EM CAMADAS:
+1. Nível Interno (Físico), que lida com o hardware e
+define como os arquivos e índices são realmente
+gravados no disco
+2. Nível Conceitual (Lógico), que descreve a
+estrutura global do banco, incluindo as tabelas,
+regras e os relacionamentos entre os dados
+3. Nível Externo (Visões), que apresenta recortes
+específicos e simplificados das informações de
+forma personalizada para diferentes usuários ou
+aplicações 
 """
