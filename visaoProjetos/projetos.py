54b1a15204666e94ca7978c26e23888cf20c6090
Matheus <matheuswschneider@gmail.com>
2026-07-27 21:52:08 -0300

    Projetos

---
+++ visaoProjetos/projetos.py
@@ -0,0 +1,25 @@
+"""
+O que faz um Gestor de
+Projetos?
+O Gestor de Projetos em dados não é um "anotador de
+recados". Ele é um tradutor.
+● Interseção entre necessidades de negócio e execução
+técnica.
+● Tradução de "Dores de Negócio" para "Soluções de
+Dados".
+● Garante que a tecnologia não seja um fim, mas um meio
+para resultado.
+
+O que define um projeto?
+● Possui uma jornada com início e fim claramente
+definidos. Não é um ciclo infinito.
+● Entrega um produto, serviço ou capacidade que não
+existia antes (ex: um novo modelo de propensão).
+● Diferente de processos repetitivos, os projetos de dados
+são elaborados em etapas, onde o detalhamento
+aumenta conforme o time avança.
+● Opera sob o equilíbrio do Triângulo de Ferro Escopo,
+Tempo e Custo), onde a alteração de um eixo impacta
+diretamente nos outros.
+
+ """
