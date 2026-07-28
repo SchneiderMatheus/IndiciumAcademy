011f812eee19cf6cb243997abade9c69dd9cc5d9
Matheus <matheuswschneider@gmail.com>
2026-07-27 21:53:55 -0300

    Pilares done

--- visaoProjetos/projetos.py
+++ visaoProjetos/projetos.py
@@ -36,4 +36,45 @@ DESENVOLVIMENTO
 E EVOLUÇÃO
 DA EQUIPE
 
+● Compreender o cliente, seu mercado, modelo de negócios, cultura organizacional,
+estratégias e partes interessadas do projeto;
+● Compreender as dificuldades do cliente, desenvolver estratégias para alcançar
+resultados com as soluções oferecidas e garantir a percepção de valor por parte
+do cliente;
+● Monitorar o progresso do projeto, criar estratégias e reportar os resultados às
+principais partes interessadas;
+● Garantir a satisfação do cliente com os serviços prestados e desenvolver planos
+para aprimorar os níveis de serviço;
+● Identificar as necessidades de dados do cliente, mapear oportunidades para
+novos desenvolvimentos (renovações) e projetos (vendas adicionais) e alinhar a
+estratégia comercial com a Gestão de Contas;
+● Priorizar e sincronizar serviços simultâneos, definindo entregas e
+cronogramas de execução;
+● Orquestrar a execução das atividades necessárias para concluir as
+entregas, garantindo que a equipe tenha a clareza e as habilidades para
+desenvolvê-las;
+● Identificar fatores de risco, gargalos do projeto e variações de
+planejamento, desenvolvendo planos de mitigação e contingência;
+● Garantir que o projeto esteja dentro do escopo, custo e prazos
+estimados, negociando-os sempre que necessário;
+● Identificar a necessidade de mudanças de direção e ajustes no projeto,
+receber, avaliar e coordenar solicitações de mudança (e,
+frequentemente, dizer não ao cliente ou fazer concessões);
+● Garantir boa comunicação e expectativas alinhadas (devidamente
+formalizadas), protegendo a equipe de desenvolvimento;
+● Gerenciar conflitos, monitorar e direcionar soluções;
+● Garantir a
+● Planejar as necessidades de pessoal do projeto e dimensionar a equipe e a
+estrutura do grupo;
+● Gerenciar uma equipe multidisciplinar para atingir os objetivos do projeto;
+● Garantir que a equipe esteja adequadamente alocada e direcionada,
+atendendo às demandas para atingir os níveis esperados de execução e
+utilização;
+● Criar e gerenciar estratégias voltadas para o cliente em conjunto com
+outros departamentos para alcançar melhor desempenho e resultados;
+● Gerenciar e alocar a equipe, considerando a evolução e o desenvolvimento
+dos indivíduos e garantindo o sucesso do projeto;
+● Medir e monitorar o desempenho da equipe, coletando informações de
+outros líderes e oferecendo feedback e orientação;
+● Contribuir para a coordenação da equipe;
  """
