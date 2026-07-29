46bc6a05c62b1f9f4882dbf49811f17feb1e484f
Matheus <matheuswschneider@gmail.com>
2026-07-29 16:21:59 -0300

    Soft Skills

---
+++ softSkills/soft.py
@@ -0,0 +1,45 @@
+""" 
+HARD VS. SOFT SKILLS
+O que você sabe vs. Quem você é
+No mercado de dados, o equilíbrio entre a técnica e o
+comportamento é o que define o sucesso de um projeto.
+● Hard Skills: Conhecimentos técnicos quantificáveis.
+● Soft Skills: Competências comportamentais e
+sociais.
+
+Hard Skills
+Linguagens de programação SQL, Python), estatística,
+ferramentas de visualização de dados.
+
+Soft Skills
+Comunicação assertiva, inteligência emocional,
+flexibilidade e resolução de conflitos.
+
+CHA
+Conhecimento + Habilidade + Atitude
+
+O Modelo de Excelência
+A competência profissional não é um ponto único, mas a união de três pilares
+fundamentais que sustentam sua entrega.
+Esse modelo define o que é ser um profissional competente. Sem Atitude, o
+Conhecimento fica estagnado.
+
+C - Conhecimento
+O "Saber". Domínio teórico de
+conceitos e métodos. Desenvolvido
+via cursos e livros.
+
+H - Habilidade
+O "Saber Fazer". A prática do
+conhecimento. Desenvolvido via
+projetos e prática.
+
+A - Atitude
+O "Querer Fazer". Forma como nos
+comportamos e encaramos os
+desafios. Desenvolvido via
+autoconhecimento.
+
+
+
+"""
